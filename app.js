183e2ecd11c7aadab07ddace4e566930a41c90a2
Vladislav Dekov <vdekoff@gmail.com>
2015-12-30 14:38:11 +0200

    Use Express.Router feature to handle with all requests.

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 var compression = require( 'compression' );
+var body_parser = require( 'body-parser' );
 var express = require( 'express' );
 var app = express();
+var router = express.Router();
+
+router.use( body_parser.json() );
+router.use( body_parser.urlencoded({
+   extended : true
+}));
 
 // Use Gzip compression
-app.use( compression() );
+router.use( compression() );
 
 // Use /dist folder to load static files
-app.use( express.static( __dirname + '/dist' ) );
+router.use( express.static( __dirname + '/dist' ) );
 
 // Handles the 'homepage' path
-app.get( '/', function ( request, response ) {
+router.get( '/', function ( request, response ) {
    response.sendFile( 'index.html', {
       root : __dirname + '/views/'
    });
 });
 
+router.post( '/', function( request, response ) {
+   response.send( request.body.foo );
+});
+
 // Create a middleware that handles a response for the 404 page
-app.use( function ( request, response, next ) {
+router.use( function ( request, response, next ) {
    response.status( 404 ).send( 'Page Not Found!' );
 });
+
+app.use( '/', router );
 
 var server = app.listen( 1337, function () {
    var host = server.address().address === '' ? server.address().address : '127.0.0.1';
